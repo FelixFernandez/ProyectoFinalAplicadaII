83c12b7ab85b6c291d6dca6b5769306ddf734def
FelixFernandez <felixradel@gmail.com>
2016-12-04 23:11:07 -0400

    compra

--- WebVentas/Registros/RegistroCompra.aspx.cs
+++ WebVentas/Registros/RegistroCompra.aspx.cs
@@ -14,43 +14,58 @@ namespace WebVentas.Registros
 
         }
 
-        public void LimpiarTextBox()
+        public void Limpiar()
         {
             TextBoxCompraID.Text = string.Empty;
             TextBoxFecha.Text = string.Empty;
-            TextBoxPrecio.Text = string.Empty;
+            TextBoxTotalCompra.Text = string.Empty;
             TextBoxCantidad.Text = string.Empty;
         }
 
-        public void LLenarTextBox()
-        {
-            Compra compra = new Compra();
-
-            compra.IdCompra = int.Parse(TextBoxCompraID.Text);
-            compra.Fecha = TextBoxFecha.Text;
-            compra.TotalCompra = float.Parse(TextBoxPrecio.Text);
-            
-        }
 
         protected void Guardar_Click(object sender, EventArgs e)
         {
             Compra compra = new Compra();
-            LLenarTextBox();
 
-            if(compra.IdCompra > 0)
+            compra.Fecha = TextBoxFecha.Text;
+            compra.TotalCompra = double.Parse(TextBoxTotalCompra.Text);
+
+
+            if (Page.IsValid)// eso es para que me valide los campos
             {
-                compra.Insertar();
-                Response.Write("guardo");
-            }
-            else
-            {
-                Response.Write("no guardo");
+                if (TextBoxCompraID.Text == "")
+                {
+                    if (compra.Insertar())
+                    {
+                        Limpiar();
+                        Response.Write("La Compra Se Guardo Correctamente");
+                    }
+                    else
+                    {
+                        Limpiar();
+                        Response.Write("La Compra no Se Guardo Correctamente");
+                    }
+                }
+                else
+                {
+                    compra.IdCompra = Convert.ToInt32(TextBoxCompraID.Text);
+                    if (compra.Modificar())
+                    {
+                        Limpiar();
+                        Response.Write("La Compra Se Modifico Correctamente");
+                    }
+                    else
+                    {
+                        Limpiar();
+                        Response.Write("La Compra no Se modifico Correctamente");
+                    }
+                }
             }
         }
 
         protected void Nuevo_Click(object sender, EventArgs e)
         {
-            LimpiarTextBox();
+            Limpiar();
         }
 
         protected void Eliminar_Click(object sender, EventArgs e)
@@ -66,6 +81,34 @@ namespace WebVentas.Registros
             else
             {
                 Response.Write("no se elimino");
+            }
+        }
+
+        private void Buscar(Compra compra)
+        {
+            TextBoxCompraID.Text = compra.IdCompra.ToString();
+            TextBoxFecha.Text = compra.Fecha.ToString();
+            DropDownListProducto.Text = compra.IdCompra.ToString();
+            DropDownListSuplidor.Text = compra.IdSuplidor.ToString();
+            TextBoxTotalCompra.Text = compra.TotalCompra.ToString();
+        }
+
+        protected void ButtonBuscar_Click(object sender, EventArgs e)
+        {
+
+            Compra compra = new Compra();
+            compra.IdCompra = int.Parse(TextBoxCompraID.Text);
+
+            if (Page.IsValid)
+            {
+                if (compra.Buscar(compra.IdCompra))
+                {
+                    Buscar(compra);
+                }
+                else
+                {
+                    Response.Write("El Id No Existe");
+                }
             }
         }
     }
